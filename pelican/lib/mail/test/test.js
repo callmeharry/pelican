c21d9048bf58bce37ab30f8c69fe853a11f5b32b
GYX <buaagyx2012@gmail.com>
2015-06-27 12:11:12 +0800

    发送和接收邮件的API（我并不想简化原来moudel中的赘余代码）

---
+++ pelican/lib/mail/test/test.js
@@ -0,0 +1,37 @@
+/**
+ * Created by GYX on 15/6/27.
+ */
+var Mail = require("../mail.js");
+
+var mail = new Mail({
+    smtphost:"smtp.buaa.edu.cn",
+    smtpport:465,
+    imaphost:"mail.buaa.edu.cn",
+    imapport:993,
+    user:"gyxln@buaa.edu.cn",
+    pass:"69568440"
+});
+
+mail.startSMTPConnection();
+var mailOptions = {
+    from: 'gyxln@buaa.edu.cn', // sender address
+    to: '450024927@qq.com', // list of receivers
+    subject: 'Hello ', // Subject line
+    text: 'Hello world ', // plaintext body
+    html: '<b>Hello world </b>' // html body
+};
+var callback =function(error, info){
+    if(error){
+        console.log(error);
+    }else{
+
+        console.log('Message sent: ' +info.message);
+    }};
+
+mail.sendMail(mailOptions,callback);
+
+mail.createMailListener(["UNSEEN"],
+    function(mail){
+        console.log(mail);
+    }
+);
