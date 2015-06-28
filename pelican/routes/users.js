de2ef90250701484a42221454870c4e909e0b890
lewiskit <lewisfyh2012@gmail.com>
2015-06-28 19:37:30 +0800

    测试数据库远程连接借口

--- pelican/routes/users.js
+++ pelican/routes/users.js
@@ -1,12 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var UserProxy = require('../proxy').User;
+
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
 
+    UserProxy.newAndSave('lewiskit', '123', 1, function (err) {
+        if (err) return next(err);
 
-    res.send('respond with a resource');
-
+        res.send('success');
+    });
 
 });
 
