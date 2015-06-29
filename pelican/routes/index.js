a4f512c3a5036caee9ee44be1c6067989b833d25
VincentBel <buaazqh@gmail.com>
2015-06-29 20:21:33 +0800

    对大部分请求进行token验证

--- pelican/routes/index.js
+++ pelican/routes/index.js
@@ -6,11 +6,45 @@ var userController = controller.user;
 
 var testApi = require('../test/api');
 
-
 /* GET TEST page. */
 router.get('/', function (req, res, next) {
     res.render('index', {title: 'Express'});
 });
+
+router.post('/login', userController.login);
+
+router.post('/test', testApi.testApi);
+
+
+// route middleware to verify a token
+router.use(function (req, res, next) {
+
+    // check header or url parameters or post parameters for token
+    var token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+    // decode token
+    if (token) {
+
+        // verifies secret and checks exp
+        jwt.verify(token, app.get('superSecret'), function (err, decoded) {
+            if (err) {
+                return res.reply(1, 'Failed to authenticate token.');
+            } else {
+                // if everything is good, save to request for use in other routes
+                req.decoded = decoded;
+                next();
+            }
+        });
+
+    } else {
+
+        // if there is no token
+        // return an error
+        return res.reply(403, 'No token provided.');
+    }
+});
+
+/*---------以下的接口都需要提供token-----------*/
 
 
 /*---------user------------*/
@@ -26,7 +60,8 @@ router.post('/login', userController.login);
 
 
 /*--------test-------------*/
-router.post('/test', testApi.testMail);
+router.post('/test', testApi.testApi);
+
 
 
 
