fda91c52d5e0a3859d288b47fd1a4e2c34666154
lewiskit <lewisfyh2012@gmail.com>
2015-06-30 00:12:43 +0800

    修改login姿势

--- pelican/controllers/user.js
+++ pelican/controllers/user.js
@@ -6,20 +6,6 @@ var validator = require('validator');
 var Log = require('../common').LogHelper;
 var jwt = require('jsonwebtoken');
 
-exports.helloUser = function (req, res, next) {
-    //UserProxy.newAndSave('lewiskit2', '123', 1, function (err) {
-    //    if (err) {
-    //        console.log("hehheda");
-    //        return next(err);
-    //    }
-    //
-    //    res.send('success');
-    //});
-    //res.send({status: 0, message: "hello world!"});
-    res.reply(0, "success", {user: "hello world"});
-
-};
-
 exports.login = function (req, res, next) {
     var username = validator.trim(req.body.username);
     var password = validator.trim(req.body.password);
@@ -30,11 +16,17 @@ exports.login = function (req, res, next) {
 
         if (err) return next(err);
 
-        if (!user || user.password != password) {
-            res.reply(101, "用户名或密码错误");
+        console.log(user.toString());
+
+        if (!user) {
+            res.reply(101, "用户不存在");
             return;
         }
 
+        if (user.password != password) {
+            res.reply(101, "用户名或密码错误");
+            return;
+        }
 
         // create a token
         var token = jwt.sign(user, req.app.get('superSecret'), {
