9308998cd8e31784ee20c27080c36c57302c6c94
GYX <buaagyx2012@gmail.com>
2015-07-05 14:27:42 +0800

    获取邮件

--- pelican/proxy/mailConfig.js
+++ pelican/proxy/mailConfig.js
@@ -46,8 +46,8 @@ exports.setConfig =function(config,callback) {
                         if (err) callback(-1, "internal error");
 
                         callback(0, 'success');
-                        Mail.clear();
-                        mailControl.openBox("INBOX", [], function (mail) {
+                        //Mail.clear();
+                        mailControl.openBox("INBOX", ["ALL"], function (mail) {
                             console.log(mail);
                             Mail.newAndSave(mail);
                         },function(err){
