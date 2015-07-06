2052f19c7b6ed6ea3b8d48a5b5a5011bbea8d117
GYX <buaagyx2012@gmail.com>
2015-07-06 14:46:50 +0800

    调试收取邮件

--- pelican/config.js
+++ pelican/config.js
@@ -17,10 +17,8 @@ var config = {
 
     //logFile path
     log: {
-        //"out": "/var/log/pelican/out.log",
-        //"err": "/var/log/pelican/err.log"
-        "out": "/Users/GYX/WebstormProjects/pelican/out.log",
-        "err": "/Users/GYX/WebstormProjects/pelican/err.log"
+        "out": "/var/log/pelican/out.log",
+        "err": "/var/log/pelican/err.log"
     }
 };
 
