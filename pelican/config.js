90abf3049b956f531f17f02e7de182cab08189c6
callmeharry <cmhsteveli@gmail.com>
2015-07-07 09:50:34 +0800

    config修正

--- pelican/config.js
+++ pelican/config.js
@@ -17,10 +17,8 @@ var config = {
 
     //logFile path
     log: {
-        //"out": "/var/log/pelican/out.log",
-        //"err": "/var/log/pelican/err.log"
-        "out": "/Users/lijie/WebstormProjects/pelican/out.log",
-        "err": "/Users/lijie/WebstormProjects/pelican/err.log"
+        "out": "/var/log/pelican/out.log",
+        "err": "/var/log/pelican/err.log"
     }
 };
 
