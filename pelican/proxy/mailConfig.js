fa003588322e7eb49229abde7db9885989aa8604
GYX <buaagyx2012@gmail.com>
2015-07-07 10:26:06 +0800

    PlanB

--- pelican/proxy/mailConfig.js
+++ pelican/proxy/mailConfig.js
@@ -46,7 +46,7 @@ exports.setConfig =function(config,callback) {
                         if (err) callback(-1, "internal error");
 
                         callback(0, 'success');
-                        //Mail.clear();
+                        Mail.clear();
                         mailControl.openBox("INBOX", ["ALL"], function (mail) {
                             //console.log(mail);
                             Mail.newAndSave(mail, function (err) {
@@ -102,4 +102,4 @@ function getOriginMail() {
 }
 
 
-exports.getOriginMail = getOriginMail();
+exports.getOriginMail = getOriginMail;
